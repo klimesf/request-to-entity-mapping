c52ee111025a2036b6c456cbefd3d5ab2f2a1a43
Filip Klimes <filip@filipklimes.cz>
2015-10-18 17:20:19 +0200

    Initialized Composer project

---
+++ src/Klimesf/RequestMapping/EntityMetadataFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Klimesf\RequestMapping;
+
+/**
+ * @package   Klimesf\RequestMapping
+ * @author    Filip Klimes <filipklimes@startupjobs.cz>
+ * @copyright 2015, Startupedia s.r.o.
+ */
+class EntityMetadataFactory
+{
+
+}
